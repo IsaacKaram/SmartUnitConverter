9c0732d9d6badd65491dd902e950d34d456d0714
Isaac Karam <isaackaram@outlook.com>
2021-03-23 03:14:31 +0200

    Finish Project

--- SmartUnitConverter/SmartUnitConverter/ContentView.swift
+++ SmartUnitConverter/SmartUnitConverter/ContentView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    private let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    @State private var inputValue = ""
+    
+    var outputValue : Double{
+        let initInput = Double(inputValue) ?? 0.0
+        return initInput
     }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section(header: Text("From :")){
+                    TextField("from value", text: $inputValue)
+                    
+                    createPicker(with: temperatureUnits, title: "Input unit", selection: $inputUnit)
+                }
+                .textCase(nil)
+                
+                
+                Section(header: Text("To :")){
+                    createPicker(with: temperatureUnits, title: "output unit", selection: $outputUnit)
+                }.textCase(nil)
+                Text("Result : \(outputValue, specifier: "%.2f") \(temperatureUnits[outputUnit])")
+                
+            }
+            .navigationBarTitle("Unit Converter")
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.green/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+func createPicker(with dataList:[String], title: String, selection : Binding<Int>) -> some View{
+   return  Picker(title, selection: selection) {
+        ForEach(0..<dataList.count){
+            Text("\(dataList[$0])")
+        }
+    }.pickerStyle(SegmentedPickerStyle())
 }
 
 struct ContentView_Previews: PreviewProvider {
